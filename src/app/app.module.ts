d59de516b1989429bf3b1243bbb8a2b25c22f58a
asbupsilonno1 <athul.babu.2300@gmail.com>
2023-06-21 03:33:33 +0530

    skill section added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { Routes,RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
-
+import { SkillsComponent } from './skills/skills.component';
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
 const routes:Routes=[
   {path:'',component:HomeComponent}
 ]
@@ -13,14 +16,15 @@ const routes:Routes=[
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
   ],
   providers: [],
-
+ schemas:[CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
