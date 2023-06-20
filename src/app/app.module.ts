db9c0fb0486b0712c7ab973952875891e471477b
asbupsilonno1 <athul.babu.2300@gmail.com>
2023-06-20 23:49:19 +0530

    added about home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { Routes,RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
+const routes:Routes=[
+  {path:'',component:HomeComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
